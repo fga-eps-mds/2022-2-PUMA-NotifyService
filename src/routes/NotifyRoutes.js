4fb4dda53d8afeedf4d241bf3a191a3a225cb4d1
Giovana Vitor Dionisio <giovanadionisio.gd@gmail.com>
2023-01-17 15:32:19 -0300

    Create initial route

--- src/routes/NotifyRoutes.js
+++ src/routes/NotifyRoutes.js
@@ -2,5 +2,12 @@
 /* eslint-disable import/no-unresolved */
 const express = require('express');
 
-const router = express.Router();
+const routes = express.Router();
 const NotifyController = require('../controller/NotifyController');
+
+routes.get('/', (req, res) => {
+  res.status(200).json({
+    Project: 'Puma',
+    Service: 'Notify-Service',
+  });
+});
